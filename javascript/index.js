23ef0e8f2508f74841121f4e23a1eccbe9edadd1
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-24 16:09:31 +0200

    Finalized iteration3 with image(bonus1)

--- javascript/index.js
+++ javascript/index.js
@@ -84,7 +84,30 @@ obtainInstruction('steak', 0)
   })
 
 // Iteration 3 using async/await
-// ...
+  async function getBroccoli () {
+    try {
+      await obtainInstruction('broccoli', 0);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[0]}</li>`
+      await obtainInstruction('broccoli', 1);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[1]}</li>`
+      await obtainInstruction('broccoli', 2);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[2]}</li>`
+      await obtainInstruction('broccoli', 3);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[3]}</li>`
+      await obtainInstruction('broccoli', 4);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[4]}</li>`
+      await obtainInstruction('broccoli', 5);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[5]}</li>`
+      await obtainInstruction('broccoli', 6);
+      document.querySelector("#broccoli").innerHTML += `<li>${broccoli[6]}</li>`
+      document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`
+      document.querySelector("#broccoliImg").removeAttribute("hidden");
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  getBroccoli();
 
 // Bonus 2 - Promise all
 // ...
