ad22ac5afdc2de2827be10c7e7c6139891056f35
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-24 16:45:58 +0200

    Finalized bonus2

--- javascript/index.js
+++ javascript/index.js
@@ -110,4 +110,15 @@ obtainInstruction('steak', 0)
   getBroccoli();
 
 // Bonus 2 - Promise all
-// ...
+Promise.all([(obtainInstruction('brusselsSprouts', 0)), (obtainInstruction('brusselsSprouts', 1)), (obtainInstruction('brusselsSprouts', 2)), (obtainInstruction('brusselsSprouts', 3)), (obtainInstruction('brusselsSprouts', 4)), (obtainInstruction('brusselsSprouts', 5)),(obtainInstruction('brusselsSprouts', 6)), (obtainInstruction('brusselsSprouts', 7)),
+])
+.then((values) => values.forEach(element => {
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>${element}</li>`
+})
+)
+.then(() => {
+  document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels Sprouts are ready!</li>`
+})
+.then(() => {
+  document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+})
